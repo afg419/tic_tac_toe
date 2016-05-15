7444ff07e75e6a72f38a86a8b9f554f8e8814e1a
Aaron Greenspan <afg419@gmail.com>
2016-05-15 15:53:53 -0600

    Seriously final commit

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,14 +2,15 @@ class Api::V1::GamesController < ApplicationController
 
   def update
     game = current_game
-    player = current_player
     pos = params["new_move"]["pos"].map(&:to_i)
-    if game && game.current_player == player && game.board[pos[0]][pos[1]] == "empty"
-      game.board[pos[0]][pos[1]] = player
+    if game && game.current_player == current_player && game.board[pos[0]][pos[1]] == "empty"
+      game.board[pos[0]][pos[1]] = current_player
       game.current_player = other_player(game.current_player)
       game.save
+
       gwc = GameWinChecker.new(game.board)
       winning_player = gwc.win? || gwc.draw?
+      
       render json: {game: game, winning_player: winning_player}
     else
       render json: {error: "no game, not your turn, or that spot's been taken already"}
