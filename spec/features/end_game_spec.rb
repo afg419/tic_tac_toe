d69b316f4c1740c9c5c8ede6191b3bce55e4ace5
Aaron Greenspan <afg419@gmail.com>
2016-05-15 15:26:23 -0600

    Adds test for replay

--- spec/features/end_game_spec.rb
+++ spec/features/end_game_spec.rb
@@ -30,4 +30,17 @@ RSpec.feature "Game ends", type: :feature do
     expect(page).to have_content("Game is a draw! Play again?")
   end
 
+
+  scenario "replay game", js: true do
+    draw_game = [["o", "x", "o"],
+                 ["x", "o", "o"],
+                 ["x", "o", "x"]]
+    g = Game.create(board: draw_game)
+    visit root_path
+    click_on "Join as X's"
+    click_on "Play again?"
+
+    expect(Game.count).to eq 0
+    expect(current_path).to eq root_path
+  end
 end
